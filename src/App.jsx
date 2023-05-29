c3a68f11f3cbebd0a9dbdca62df4f8fbae5cbcd1
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-05-28 21:38:16 -0300

    Ejercicio 1 react completo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HolaMundo from './components/HolaMundo'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+    <HolaMundo></HolaMundo>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,8 @@
+
+const HolaMundo = () => {
+    return (
+        <h1 className="text-center my-2">Hola mundo en js con react</h1>
+    );
+};
+
+export default HolaMundo;
